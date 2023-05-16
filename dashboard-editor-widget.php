4a801970900f48c0fdac20fd02437ad7a7910839
Toni Ginard <toni.ginard@gmail.com>
2023-05-16 17:09:19 +0200

    Improved widget content

--- dashboard-editor-widget.php
+++ dashboard-editor-widget.php
@@ -48,12 +48,22 @@ add_action('wp_dashboard_setup', function () {
 function dashboard_widget_editor() {
     $tadv_admin_settings = get_option(OPTIONS_KEY);
 
-    echo '<p>El Gutenberg és un editor de contingut modern i avançat que permet crear contingut visualment atractiu i 
-             altament personalitzat, utilitzant blocs de diferents tipus com ara títols, text, imatges, vídeos, 
-             diapositives, etc. i disposar-los de forma fàcil i intuïtiva.</p>
-          <p>En cas de desactivar el Gutenberg, algun contingut generat amb aquest pot no visualitzar-se correctament 
-             amb l\'editor clàssic. Canviar a l\'editor clàssic només es recomana si cal canviar, dins d\'algun article 
-             o pàgina, contingut dels Carrusels o les Pestanyes.</p>';
+    echo '<div style="display:flex; align-items:flex-start;">
+              <div style="flex:1; margin-bottom:10px;">
+                  <p>
+                      El Gutenberg és el nou editor de contingut del WordPress. Funciona amb blocs, cosa que permet tenir un 
+                      bon control sobre la maquetació dels continguts del web.
+                  </p>
+                  <p>
+                      <a href="https://projectes.xtec.cat/digital/serveis-digitals/nodes/editor-gutenberg/#compat-carrusels-pestanyes"
+                         target="_blank">Compatibilitat amb l\'editor clàssic, els Carrusels i les Pestanyes</a>
+                  </p>
+              </div>
+              <div style="width:100px; margin-left:10px;">
+                  <img src="' . plugin_dir_url( __FILE__ ) . '/gutenberg-logo.svg" width="100" height="100"
+                      alt="Gutenberg Logo"/>
+              </div>
+          </div>';
 
     // Check if the option is set.
     if (strpos($tadv_admin_settings['options'], TINYMCE_KEY) !== false) {
@@ -61,6 +71,10 @@ function dashboard_widget_editor() {
     } else {
         echo '<a class="button button-primary" href=' . admin_url() . '?dashboard-editor-widget=deactivate>Desactiva</a>';
     }
+
+    echo '&nbsp;&nbsp;&nbsp;';
+    echo '<a class="button button-secondary" href="https://projectes.xtec.cat/digital/serveis-digitals/nodes/editor-gutenberg/"
+            target="_blank">Més informació</a>';
 }
 
 /**
